b76627582751aa5c274b393e64f684378b8bd59b
Martin Heredia <martinheredia.dev@gmail.com>
2022-08-11 04:33:20 -0300

    fixed

--- 20vo entregable/api/controllers/UserController.js
+++ 20vo entregable/api/controllers/UserController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = {
   get: async (req, res) => {
@@ -12,40 +12,41 @@ module.exports = {
       const users = await User.find();
       if (!users) {
         return res.status(404).send({
-          message: 'No users found',
+          message: "No users found",
         });
       }
       return res
         .status(200)
-        .send({ success: true, message: 'Users found', data: users });
+        .send({ success: true, message: "Users found", data: users });
     } catch (err) {
       return res.status(500).json(err);
     }
   },
   create: async (req, res) => {
     try {
-      const image = req.file('picture').upload({ dirname: '../../assets/images/users' }, async (err, uploadedFiles) => {
-        if (err) {
-          return res.status(500).json(err);
-        }
-        
-      }
-      );
-      const userData = {
-        name: req.body.name,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-        phone: req.body.phone,
-        address: req.body.address,
-        picture: image,
-      };
-      const user = await User.create(userData);
-      return res
-        .status(200)
-        .send({ success: true, message: 'User created', data: user });
+      req
+        .file("picture")
+        .upload(
+          { dirname: "../../assets/images/users" },
+          async (err, uploadedFiles) => {
+            if (err) return res.serverError(err);
+            if (uploadedFiles.length === 0) {
+              return res.badRequest("No file was uploaded");
+            }
+            sails.log.info(uploadedFiles[0]);
+            const user = await User.create({
+              ...req.body,
+              picture: 'uploaded'+ uploadedFiles[0].filename,
+            });
+            return res.status(200).send({
+              success: true,
+              message: "User created",
+              data: user,
+            });
+          }
+        );
     } catch (err) {
       return res.status(500).json(err);
     }
-  }
+  },
 };
